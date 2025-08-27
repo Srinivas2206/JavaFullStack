bfa75f894de5eeebe2eab4c2da75430b835f1f1a
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2025-08-27 20:15:51 +0530

    Abstraction, Inheritance, Packages

---
+++ src/Abstraction-and-Packages/Packages.java
@@ -0,0 +1,11 @@
+import test.Foundation;
+
+public class Packages {
+    public static void main(String[] args) {
+        Foundation fd = new Foundation();
+        // System.out.println(fd.var1);
+        // System.out.println(fd.var2);
+        // System.out.println(fd.var3);
+        System.out.println(fd.var4);
+    }
+}
---
+++ src/Abstraction-and-Packages/test/Foundation.java
@@ -0,0 +1,20 @@
+package test;
+/*
+ * Create a package called test package.
+ * Define a class called foundation inside the test package.
+ * Inside the class, you need to define 4 integer variables:
+ * var1 with private access modifier
+ * var2 with default access modifier
+ * var3 with protected access modifier
+ * var4 with public access modifier
+ * Import this class and packages in another class. 
+ * Try to access all 4 variables of the foundation class and 
+ * see what variables are accessible and what are not accessible.
+ */
+
+public class Foundation {
+    private int var1 = 1;
+    int var2 = 2;
+    protected int var3 = 3;
+    public int var4 = 4;
+}
